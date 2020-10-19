c4ac74ca86cd5b08e7e6724b737c2c4da7048d0b
Harry Carron <hcarron92@gmail.com>
2020-10-19 23:20:35 +0100

    Update matrix2.component.ts
    mainly chnages to function which gets nodes to be animated

--- src/app/components/matrix2/matrix2.component.ts
+++ src/app/components/matrix2/matrix2.component.ts
@@ -7,6 +7,12 @@ import {
   NgZone,
 } from "@angular/core";
 
+enum NodeMode {
+  on,
+  off,
+  animate,
+}
+
 @Component({
   selector: "app-matrix2",
   templateUrl: "./matrix2.component.html",
@@ -20,9 +26,11 @@ export class Matrix2Component implements OnInit, AfterViewInit {
 
   private nodeDimension: number;
 
-  readonly NOTE_ON_COL = "white";
+  readonly NODE_ON_COL = "white";
 
-  readonly NOTE_OFF_COL = "#252c33";
+  readonly NODE_OFF_COL = "#252c33";
+
+  readonly NODE_ANIMATE_COL = "#45525F";
 
   private activeNodes: any[] = [];
 
@@ -69,11 +77,20 @@ export class Matrix2Component implements OnInit, AfterViewInit {
     this.initNodes();
   }
 
-  private toggleNode(nodeID: [number, number], isOn: boolean): void {
+  private toggleNode(nodeID: [number, number], nodeMode: NodeMode): void {
     this.canvasDraw.beginPath();
-    this.canvasDraw.fillStyle = isOn ? this.NOTE_ON_COL : this.NOTE_OFF_COL;
+    switch (nodeMode) {
+      case NodeMode.on:
+        this.canvasDraw.fillStyle = this.NODE_ON_COL;
+        break;
+      case NodeMode.off:
+        this.canvasDraw.fillStyle = this.NODE_OFF_COL;
+        break;
+      case NodeMode.animate:
+        this.canvasDraw.fillStyle = this.NODE_ANIMATE_COL;
+    }
 
-    if (!isOn) {
+    if (nodeMode === NodeMode.off) {
       this.canvasDraw.clearRect(
         this.nodeDimension * nodeID[0],
         this.nodeDimension * nodeID[1],
@@ -88,9 +105,6 @@ export class Matrix2Component implements OnInit, AfterViewInit {
       this.nodeDimension - this.PAD * 2
     );
     this.canvasDraw.stroke();
-    if (isOn) {
-      this.beginAnimation(nodeID);
-    }
   }
   canvasClicked(event) {
     const domRect = this.canvas.getBoundingClientRect();
@@ -110,27 +124,213 @@ export class Matrix2Component implements OnInit, AfterViewInit {
 
     if (index === -1) {
       this.activeNodes.push(key);
-      this.toggleNode([clickedX, clickedY], true);
+      this.toggleNode([clickedX, clickedY], NodeMode.on);
+      this.beginAnimation([clickedX, clickedY]);
     } else {
       this.activeNodes.splice(index, 1);
-      this.toggleNode([clickedX, clickedY], false);
+      this.toggleNode([clickedX, clickedY], NodeMode.off);
     }
   }
 
-  private getAnimationRun() {}
+  private getAnimationNodes(node: [number, number], radius: number): any {
+    let nodes = [];
+    if (radius === 1) {
+      return nodes = [
+        () => {
+          const _node = [...node];
+          _node[1] = _node[1] - 1; // above
+          return _node;
+        },
+        () => {
+          const _node = [...node];
+          _node[0] = _node[0] + 1; // right
+          return _node;
+        },
+        () => {
+          const _node = [...node];
+          _node[1] = _node[1] + 1; // below
+          return _node;
+        },
+        () => {
+          const _node = [...node];
+          _node[0] = _node[0] - 1; // left
+          return _node;
+        }
+      ].map(f => f()).filter((n) => !this.nodeIsActive(n as [number, number]));
+    } else {
+      const a = (v) => Array.from(Array(v).keys());
+
+
+      const startPoint = Math.floor(radius / 2);
+
+      node[1] = node[1] - radius;
+      node[0] = node[0] - startPoint;
+
+      const isOdd = n => n % 2;
+
+      a(radius).forEach((i) => {
+        const _node = [...node];
+        _node[0] = node[0] + (i + 1);
+        nodes.push(_node);
+      });
+
+      const rightNodes = [...node];
+      rightNodes[1] = rightNodes[1] - startPoint;
+      rightNodes[0] = rightNodes[0] + radius;
+
+      a(radius).forEach(_ => {
+        rightNodes[1] = rightNodes[1] + 1;
+        nodes.push(rightNodes);
+      });
+
+      const bottomNodes = [...node];
+      bottomNodes[0] = bottomNodes[0] - startPoint;
+      bottomNodes[1] = bottomNodes[1] + radius;
+
+      a(radius).forEach(_ => {
+        bottomNodes[1] = bottomNodes[1] + 1;
+        nodes.push(bottomNodes);
+      });
+
+      const leftNodes = [...node];
+      leftNodes[1] = leftNodes[1] - startPoint;
+      leftNodes[0] = leftNodes[0] - radius;
+
+      a(radius).forEach(_ => {
+        leftNodes[1] = leftNodes[1] + 1;
+        nodes.push(leftNodes);
+      });
+
+
+      return nodes.filter((n) => !this.nodeIsActive(n as [number, number]));
+    }
+    // switch (run) {
+    //   case 1: {
+
+    //     ];
+    //   } break;
+    //   case 2: {
+    //     nodes = [
+    //       () => {
+    //         const _node = [...node];
+    //         _node[1] = _node[1] - 2; // above
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[0] = _node[0] + 1;
+    //         _node[1] = _node[1] - 2; // above right
+
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[0] = _node[0] + 2;
+    //         _node[1] = _node[1] - 1; // right above
+
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[0] = _node[0] + 2; // right
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[0] = _node[0] + 2; // below right
+    //         _node[1] = _node[1] + 1;
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[0] = _node[0] + 1; // right below
+    //         _node[1] = _node[1] + 2;
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[1] = _node[1] + 2; // below
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[1] = _node[1] + 2; // below left
+    //         _node[0] = _node[0] - 1;
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[0] = _node[0] - 2;
+    //         _node[1] = _node[1] + 1; // left below
+
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[0] = _node[0] - 2; // left
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[0] = _node[0] - 2;
+    //         _node[1] = _node[1] - 1; // left above
+    //         return _node;
+    //       },
+    //       () => {
+    //         const _node = [...node];
+    //         _node[0] = _node[0] - 1;
+    //         _node[1] = _node[1] - 2; // above left
+    //         return _node;
+    //       },
+
+    //     ];
+    //   }
+    // }
+
+  }
 
   private beginAnimation(node: any): void {
     let requestId = null;
-    let time = 1; // todo nmake dynamic!!!
-    const interval = 0.05;
-    let stage = 1;
+    let time = 1;
+    const interval = 0.20;
+    let stage = 2;
+    let initaliseStage = true;
+    let animationNodes = [];
 
     const animate = () => {
-      if (stage === 1) {
+      // if (stage === 1) {
+      //   if (initaliseStage) {
+      //     animationNodes = this.getAnimationNodes(node, 1);
+      //     initaliseStage = false;
+      //     animationNodes.forEach((an) => this.toggleNode(an, NodeMode.animate));
+      //   }
+      //   time = time - interval;
+      //   if (time <= 0) {
+      //     stage = 2;
+      //     initaliseStage = true;
+      //     animationNodes.forEach((an) => this.toggleNode(an, NodeMode.off));
+      //   }
+      // }
+
+      if (stage === 2) {
+        if (initaliseStage) {
+          time = 1;
+          animationNodes = this.getAnimationNodes(node, 2);
+          initaliseStage = false;
+          animationNodes.forEach((an) => this.toggleNode(an, NodeMode.animate));
+        }
+        time = time - interval;
+        if (time <= 0) {
+          stage = 3;
+          /// animationNodes.forEach((an) => this.toggleNode(an, NodeMode.off));
+        }
       }
-      requestId = requestAnimationFrame(animate);
-      if (stage === 4) {
+
+      if (stage === 3) {
         cancelAnimationFrame(requestId);
+        console.log('cancelled');
+      } else {
+        requestId = requestAnimationFrame(animate);
       }
     };
 
@@ -139,18 +339,14 @@ export class Matrix2Component implements OnInit, AfterViewInit {
     });
   }
 
+  private nodeIsActive(node: [number, number]): boolean {
+    return this.activeNodes.indexOf(node.join("-")) > -1;
+  }
+
   private initNodes() {
     this.rowIDs.forEach((rowID, rowI) => {
       this.rowIDs.map((colID, colI) => {
-        this.canvasDraw.beginPath();
-        this.canvasDraw.fillStyle = this.NOTE_OFF_COL;
-        this.canvasDraw.fillRect(
-          this.nodeDimension * colI + this.PAD,
-          this.nodeDimension * rowI + this.PAD,
-          this.nodeDimension - this.PAD * 2,
-          this.nodeDimension - this.PAD * 2
-        );
-        this.canvasDraw.stroke();
+        this.toggleNode([colI, rowI], NodeMode.off);
       });
     });
   }
